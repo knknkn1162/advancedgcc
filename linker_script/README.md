534e7e4fb8ed7cfad4238341fe6c274a4d81e98e
knknkn1162 <knknkn1162@gmail.com>
2020-01-12 16:08:11 +0900

    add .bss example in linker_script

--- linker_script/README.md
+++ linker_script/README.md
@@ -3,3 +3,5 @@
 + You can check default linker script by `ld --verbose`. See https://gist.github.com/knknkn1162/d7301e6a14d8a781bf6a61ceb7985e9c
 
 + If you want to specify entrypoint function name, write `ENTRY($func_name)` on linker script or `-e $func_name` in gcc option.
+
++ try `make NUM=4`. Compiler does initialize zeros in bss section!
--- linker_script/main.c
+++ linker_script/main.c
@@ -1,7 +1,16 @@
 #include <unistd.h>
 
+#ifndef NUMBER
+#define NUMBER 0
+#endif
+// If but 0, error: only zero initializers are allowed in section '.bss'
+// Compiler does initialize zeros in bss section!
+int num __attribute__ ((section (".bss"))) = NUMBER;
+
 void test(void) {
   const char name[] = "Kenta\n";
   write(STDOUT_FILENO, name, sizeof(name));
+  const char n_str[3] = {num+'0', '\n', '\0'};
+  write(STDOUT_FILENO, n_str, sizeof(n_str));
   _exit(0);
 }
